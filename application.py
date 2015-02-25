c244f7322e0520701035b3d8fce6f363229f03fa
dasimao <dasimao@impresa.pt>
2015-02-25 16:40:19 +0000

    uninstall tweepy

--- application.py
+++ application.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import requests
+from requests_oauthlib import OAuth1
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
+    url = 'https://api.twitter.com/1.1/search/tweets.json?q=%23charliehebdo&lang=pt'
+    auth = OAuth1('hxyttWQ5OuciWPJdBJgntZ3UO', 'oJFPWpLuuNM3hjfpTLhKCMOdjHo5gQktdzTfEK5BaetznEn5dK',
+                  '22373407-72aJvjhjUH7BZ3J9kJjeuv0wrWBWh4cRxCfNUGG89', 'zA3YWLSxJr0BgvGnh5Ca9QXKhiumSUyTi5pYvxUJ7nNvh')
+    r=requests.get(url, auth=auth)
+    return r.text
 
 if __name__ == '__main__':
 	app.run()
